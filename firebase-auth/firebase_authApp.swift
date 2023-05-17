f978ade67e6d6a55c74c9a851016cabf9d4d8e79
irfanizudin <irfanizzudyn@gmail.com>
2023-05-17 20:57:55 +0700

    Initial Commit

---
+++ firebase-auth/firebase_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_authApp.swift
+//  firebase-auth
+//
+//  Created by Irfan Izudin on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct firebase_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
